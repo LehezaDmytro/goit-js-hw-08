0d637d7e4294bfe1474804c879d727a38a2a8d1b
Leheza Dmytro <legeza.dmitriy@icloud.com>
2023-01-13 01:52:33 +0200

    Виконано завдання №1 і №2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,16 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
+const gallery = document.querySelector('.gallery')
 
-console.log(galleryItems);
+galleryItems.forEach(({ preview, original, description }) => {
+    gallery.insertAdjacentHTML('beforeend',
+        `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`)
+});
+
+const clickOnThePicture = new SimpleLightbox('.gallery__item', { captionDelay: 500, captionsData: 'alt'});
+clickOnThePicture.on('show.simplelightbox', function () {});
