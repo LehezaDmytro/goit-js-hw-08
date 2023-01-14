49d06af5fe521470d054ddc22eab7279dd0afcb4
Leheza Dmytro <legeza.dmitriy@icloud.com>
2023-01-14 18:36:43 +0200

    Виправлено помилки

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,32 +4,41 @@ const form = document.querySelector('.feedback-form');
 const input = document.querySelector('.feedback-form input');
 const textarea = document.querySelector('.feedback-form textarea')
 
+let parseLocalStorage = JSON.parse(localStorage.getItem('feedback-form-state'))
+
 form.addEventListener('input', saveFormValues)
 
-const formValues = { email: "", message: "",}
-
 function saveFormValues(event) {
+    const formValues = { email: "", message: "", }
     const { elements: { email, message } } = event.currentTarget;
+
     formValues.email = email.value;
     formValues.message = message.value;
+
     localStorage.setItem('feedback-form-state', JSON.stringify(formValues));
+    parseLocalStorage = JSON.parse(localStorage.getItem('feedback-form-state'))
 }
 
-const parseLocalStorage = JSON.parse(localStorage.getItem('feedback-form-state'))
-input.value = parseLocalStorage.email;
-textarea.value = parseLocalStorage.message;
+if (parseLocalStorage) {
+    input.value = parseLocalStorage.email;
+    textarea.value = parseLocalStorage.message;
+} else {
+    input.value = "";
+    textarea.value = "";
+}
 
 form.addEventListener('click', throttle(handleSubmit, 500));
 
 function handleSubmit(event) {
-    if (event.target.nodeName !== "BUTTON") {
+    if (event.target.nodeName !== "BUTTON" || !parseLocalStorage) {
         return
+    } else if (input.value === "" || textarea.value === "") {
+        alert("Please fill in all the fields!")
     } else {
-        // const { elements: { email, message } } = event.currentTarget;
         console.log(parseLocalStorage);
-    // console.log(message.value);
         form.reset();
-        localStorage.removeItem('feedback-form-state')
+        localStorage.removeItem('feedback-form-state') 
+        parseLocalStorage = undefined;
     }
 }
 
